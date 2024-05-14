e447a666447a5d38c1a011b469f0f80630058bdc
root <you@example.comsamuelomotayosamuel@gmail.com>
2024-05-14 12:52:36 -0700

    Learning python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-        if chr(char) not in ['q', 'e']:
-                    print(chr(char), end='')
+for x in range(97, 123):
+        if x != 101 and x != 113:
+                    print("{:c}".format(x), end="")
