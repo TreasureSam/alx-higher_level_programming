5dbaa23bb132d2008fd688439362cedfbfb22bd6
root <you@example.comsamuelomotayosamuel@gmail.com>
2024-05-14 12:40:53 -0700

    Learning python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if char != 101 and char !=113:
-        print("{:c}".format(char), end='')
+for x in range(97, 123):
+    if x != 101 and x !=113:
+        print("{:c}".format(x), end='')
