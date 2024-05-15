f1a38dbd2f0728aad072e3bc04c2f3be0758e1a5
root <you@example.comsamuelomotayosamuel@gmail.com>
2024-05-15 13:43:06 -0700

    Learning python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
-        for units_digit in range(tens_digit + 1, 10):
-                    print("{:d}{:d}".format(tens_digit, units_digit), end=", " if tens_digit < 9 or units_digit < 9 else "\n")
+        for ones_digit in range(tens_digit + 1, 10):
+            if tens_digit == 9 and ones_digit == 9:
+                print("{:02d}".format(tens_digit * 10 + ones_digit))
+            else:
+                print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
