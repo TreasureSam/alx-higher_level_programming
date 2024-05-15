b183caeef3811e203c0cf9d2148b1b75594c60c3
root <you@example.comsamuelomotayosamuel@gmail.com>
2024-05-15 13:21:15 -0700

    Leanring python

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for tens_digit in range(10):
+        for units_digit in range(tens_digit + 1, 10):
+                    print("{:d}{:d}".format(tens_digit, units_digit), end=", " if tens_digit < 9 or units_digit < 9 else "\n")
