22515c0f09580158be7d9c18f0aea8c066dc8394
root <you@example.comsamuelomotayosamuel@gmail.com>
2024-05-15 13:08:53 -0700

    Learning python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for x in range(0, 100):
-        print("{:02d}".format(x), end=", " if x < 100 else "\n")
+for x in range(0, 99):
+        print("{:02d}".format(x), end=", " if x < 99 else "\n")
